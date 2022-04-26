da105ee98e9cdfeda6b91fbe7aa14909921bae88
permingeat <laetitia.permingeat@le-campus-numerique.fr>
2022-04-26 11:11:48 +0200

    commit du 26/04

--- drive.js
+++ drive.js
@@ -1,7 +1,6 @@
 import os from 'os'
 import fs from 'fs/promises'
 import { join } from 'path'
-import { response } from 'express';
 
 const DRIVE_ROOT = join(
     os.tmpdir(),
@@ -12,25 +11,29 @@ console.log("Dossier racine du AlpsDrive: ", DRIVE_ROOT);
 
 
 export function listeDeDossiers(path) {
-    const folder = join(DRIVE_ROOT, path)
-    console.log(folder);
+    const concatenatedPath = join(DRIVE_ROOT, path)
+    console.log(concatenatedPath);
 
-    return fs.readdir(folder, { withFileTypes: true })
-    //J'attends que toutes mes promesses soient terminées
-        .then(direntItems => Promise.all(direntItemsToFiles(direntItems,folder)))
+    return fs.readdir(concatenatedPath, { withFileTypes: true })
+        //J'attends que toutes mes promesses soient terminées
+        .then(direntItems => Promise.all(direntItemsToFiles(direntItems, concatenatedPath)))
         //Je retourne mes promesses, arrayDatas correspond à response, 
         //la réponse du then
-        .then(arrayDatas=>arrayDatas)
-
+        .then(arrayDatas => arrayDatas)
 }
 
+export function readFile(path) {
+    const concatenatedPath = join(DRIVE_ROOT, path);
+    return fs.readFile(concatenatedPath);
+
+}
 
 //retourne les infos sur mes éléments
 export function direntItemsToFiles(direntItems, path) {
     return direntItems.map(direntItem => {
         console.log(direntItems);
         //Je regarde si mon item est un dossier
-        if (direntItem.isDirectory() == true) {
+        if (direntItem.isDirectory() === true) {
 
             let fileObject = {
                 name: direntItem.name,
@@ -38,23 +41,27 @@ export function direntItemsToFiles(direntItems, path) {
             }
             return fileObject
 
-//s'il est pas un dossier
+            //s'il est pas un dossier
         } else {
             return fs.stat(path)
                 .then(objStats => {
+                    console.log("la réponse", objStats);
                     const fileObject = {
                         name: direntItem.name,
                         isFolder: direntItem.isDirectory(),
                         size: objStats.size
                     }
-
                     return fileObject
                 })
-
-
         }
     })
 }
 
+//Création d'un dossier
+export function addFolder(path) {
+    const concatenatedPath = join(DRIVE_ROOT, path)
+    return fs.mkdir(concatenatedPath)
+        .catch(console.log("Il y a une erreure"))
+}
 
 
--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import { fstat } from 'fs'
 import * as drive from './drive.js'
 
 //import { append } from 'express/lib/response'
@@ -11,42 +10,66 @@ export function start() {
     app.get('/', (req, res) => {
         res.render('index.html')
     })
-    /*  app.get('/api/drive', (req, res) => {
-          res.writeHead(200, {
-              'content-type': 'application/json'
-          });
-          res.send([
-              {
-                  name: "Personnel",
-                  isFolder: true,
-              },
-              {
-                  name: "Avis imposition",
-                  size: 1337,
-                  isFolder: false
-              }
-          ])
-  
-      })*/
-
 
     //affiche les dossiers
     app.get('/api/drive', (req, res) => {
         res.status(200, { 'Content-Type': 'application/json' });
         drive.listeDeDossiers("")
-            .then(files => { 
+            .then(files => {
                 console.log(files);
-                res.send(files) 
+                res.send(files)
             })
         //.then(files => res.status(200).json(files))
     })
 
-    //affiche les fichiers qu'il y a dans les dossiers
+    //affiche le contenu des dossiers ou des fichiers
     app.get('/api/drive/:name', (req, res) => {
+        console.log("params : ",req);
+
+        drive.listeDeDossiers("")
+            .then(content => {
+                content.forEach(element => {
+                    if (element.name === req.params.name) {
+                        //si c'est un dossier, alors on l'ouvre
+                        if (element.isFolder) {
+                            //res.status(200, { 'Content-Type': 'application/json' });
+                            drive.listeDeDossiers(req.params.name)
+                                .then(directory => {
+                                    res.status(200).json(directory)
+                                })
+                            //sinon si c'est un fichier on affiche son contenu
+                        } else {
+                            console.log("chemin",req.params.name);
+                            drive.readFile(req.params.name)
+                                .then(file => {
+                                    res.setHeader('Content-Type', 'application/octet-stream')
+                                    res.status(200).send(file)
+                                })
+                        }
+                    }
+                })
+            })
+    })
+
+        //AJouter un élément
+        app.post('/api/drive/', (req, res) => {
+           // console.log("params post : ",req.query.name);
+            drive.addFolder(req.query.name)
+            .then(()=> {
+                res.status(201).send()
+            })
+    
+           
+        })
+
+    
+
+    //affiche les fichiers qu'il y a dans les dossiers
+    /*app.get('/api/drive/:name', (req, res) => {
         res.status(200, { 'Content-Type': 'application/json' });
         drive.listeDeDossiers(req.params.name)
             .then(files => res.send(files))
-    })
+    })*/
 
     app.listen(port, () => {
         console.log(`Example app listening on port ${port}`)
